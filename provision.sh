be21634a626c70b13264440b5c2772238f14f9c9
Kamil Ciemniewski <kamil@endpoint.com>
2020-03-23 17:11:40 +0100

    WIP

--- provision.sh
+++ provision.sh
@@ -3,6 +3,6 @@
 apt-get upate
 apt-get install unzip
 
-conda install -y jupyterlab hypothesis pandas transformers conda-forge spacy tqdm ipywidgets
+conda install -y jupyterlab hypothesis pandas transformers conda-forge spacy tqdm ipywidgets fastparquet
 python -m spacy download en_core_web_lg scikit-learn
 pip install plotnine hickle
